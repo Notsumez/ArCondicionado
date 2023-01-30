8d2e70e40303ca52dd26c5093f0e603851766a57
notsumez <adahorapirola@gmail.com>
2023-01-30 16:24:00 -0300

    Header Feito

--- index.php
+++ index.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Link do css e reset -->
+    <link rel="stylesheet" href="CSS/reset.css">
+    <link rel="stylesheet" href="CSS/estilo.css">
+    <!-- Link do Angular -->
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
     <title>Air-Dited</title>
 </head>
 <body>
-    
+    <!-- incluuindo o header -->
+    <?php include('header.html');?>
+
+    <main>
+        <figure>
+            <img class="banner-energia-solar" src="./images/energia_solar.png" alt="Imagem Energia Solar">
+        </figure>
+    </main>    
 </body>
 </html>
