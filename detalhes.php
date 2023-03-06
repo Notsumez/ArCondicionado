5e15a554afe910b69865a3c8aa85acbff3022e56
Andrey da Hora Pirola <adahorapirola@gmail.com>
2023-03-05 21:59:01 -0300

    mudanças

--- detalhes.php
+++ detalhes.php
@@ -14,7 +14,7 @@
     <!-- Link do css e reset -->
     <link rel="stylesheet" type="text/css" href="CSS/reset.css">
     <link rel="stylesheet" type="text/css" href="CSS/estilo.css">
-    <link rel="stylesheet" type="text/css" href="CSS/all.min.css">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
     <link rel="shortcut icon" href="./images/logo-shorticon.png" type="image/png">
     <!-- Link do Angular -->
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
@@ -24,14 +24,15 @@
     <?php include 'header.html';?>  
     <div id="container-detalhes">
         <!-- Top para voltar -->
-        <h2 class="btn-voltar" >
+        <h2>
             <a href="javascript:window.history.go(-1)" role="button" >
-                <span>
+                <span class="btn-voltar">
                     <i class="fa-solid fa-circle-chevron-left"></i>
                 </span>
-            </a>   
+            </a>&nbsp;
             Detalhes de <?php echo $row['nome']; ?>
         </h2>             
+        
         <?php include 'footer.html';?>
 </body>
 </html>
